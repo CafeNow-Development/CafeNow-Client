829fec69ab37cf6a5c85a46b49f7d6c4363fac04
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-13 16:45:47 +0900

    chore: default setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Router from "next/router";
+import React, { useEffect } from "react";
+
+const HomeKeeper: React.FC = () => {
+  return (
+    <>
+      <div>카페 나우</div>
+    </>
+  );
+};
+
+export default HomeKeeper;
