7e2345552e9c1c2d540dfe2666bbe85b8437fc61
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-15 17:35:01 +0900

    chore: GlobalStyle

---
+++ style/GlobalStyle.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: "Jua";
+        margin: 0;
+        padding : 0;
+    }
+    button:focus,
+    input:focus {
+        outline: none;
+    }
+    button {
+        cursor: pointer;
+    }
+`;
