63a25e6e7079142ff582289246d4403b6e612524
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-20 12:57:54 +0900

    feat: SearchPage in SearchBar publish

---
+++ components/Search/SearchBar/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import * as S from "./style";
+
+import * as I from "../../../assets";
+
+const SearchBar: React.FC = () => {
+  return (
+    <S.SearchBarWrapper>
+      <I.BackIcon />
+      <S.SearchBar placeholder="카페 이름 또는 위치를 검색해보세요." />
+    </S.SearchBarWrapper>
+  );
+};
+
+export default SearchBar;
