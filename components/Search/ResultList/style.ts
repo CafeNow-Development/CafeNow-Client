7b7855993f1ad4bc577fed1d37baa2893e427584
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-20 12:58:35 +0900

    feat: SearchPage in ResultList publish

---
+++ components/Search/ResultList/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const ResultListWrapper = styled.div`
+    display:  flex;
+    flex-direction: column;
+
+    margin-top : 17px;
+
+`;
+
+export const ResultItem = styled.div`
+    padding : 9px 0;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 16px;
+
+    color: #000000;
+`;
