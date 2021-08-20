f6f788571be00e4cf582056b6b83692f378b457d
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-20 11:23:47 +0900

    feat: StartPage in Header publish

---
+++ components/Start/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from "./style";
+
+import * as I from "../../../assets";
+
+const Header: React.FC = () => {
+  return (
+    <S.HeaderWrapper>
+      <I.Logo />
+    </S.HeaderWrapper>
+  );
+};
+
+export default Header;
