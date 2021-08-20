3cb969d564616f6317fa2d4e85eb26619a043649
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-20 11:23:24 +0900

    chore: search input icon import

---
+++ assets/SearchIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchIcon: React.FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="21" fill="none" viewBox="0 0 19 21">
+      <path stroke="#9E9E9E" strokeWidth="2" d="M12.768 14.36L17.768 20.36"></path>
+      <circle cx="8.5" cy="8.5" r="7.5" stroke="#9E9E9E" strokeWidth="2"></circle>
+    </svg>
+  );
+};
+
+export default SearchIcon;
