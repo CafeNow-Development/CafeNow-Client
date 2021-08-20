2268e5c5ad80c0052ae14b8b53ffb8e771aedb07
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-20 11:24:54 +0900

    feat: StartPage Publish

--- components/Start/index.tsx
+++ components/Start/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Header from "./Header";
+import SearchBar from "./SearchBar";
+
+import * as S from "./style";
+import TagList from "./TagList";
 
 const Start: React.FC = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <S.Positioner>
+      <Header />
+      <S.MainTitleWrapper>
+        <S.MainTitleText>
+          카페 어디서나 주문을,
+          <br />
+          카페나우
+        </S.MainTitleText>
+      </S.MainTitleWrapper>
+      <SearchBar />
+      <TagList />
+    </S.Positioner>
   );
 };
 
