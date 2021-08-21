a9843e6876c20b83b48f3cce8199a1c9558bce72
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-21 18:26:54 +0900

    feat: detailPage page publish

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export { default as Start } from "./Start"; 
 export { default as Search } from "./Search"
 export { default as Result } from "./Result"
+export { default as StoreDetail } from "./StoreDetail"
+export { default as KaKaoMap } from "./KaKaoMap"
---
+++ pages/result/store/[storeId].tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { StoreDetailContainer } from "../../../containers";
+
+const DetailStore: React.FC = () => {
+  return (
+    <>
+      <StoreDetailContainer />
+    </>
+  );
+};
+
+export default DetailStore;
