9852ed00416bcee3e35b3b1f4542f5c893755ae5
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-25 11:46:32 +0900

    feat: order page dummy data 생성

--- components/Order/index.tsx
+++ components/Order/index.tsx
@@ -1,12 +1,46 @@
 import React from "react";
+import ImageSlider from "../ImageSlider";
 
-interface OrderProps {}
+import * as S from "./style";
 
-const Order: React.FC<OrderProps> = () => {
+import * as I from "../../assets";
+import MenuList from "./MenuList";
+import ReviewList from "./ReviewList";
+
+interface MenuItemProps {
+  menuId: number;
+  menuName: string;
+  menuPrice: number;
+  menuImage: string;
+}
+
+interface OrderProps {
+  nav: string;
+  setNav: (nav: string) => void;
+  menuList: MenuItemProps[];
+}
+
+const Order: React.FC<OrderProps> = ({ menuList, nav, setNav }) => {
   return (
-    <div>
-      <div>OrderPage</div>
-    </div>
+    <S.Positioner>
+      <ImageSlider />
+      <S.Wrapper>
+        <S.OrderCafeInfoWrapper>
+          <S.CafeNameText>스타벅스 광산 수완점</S.CafeNameText>
+          <S.StarWrapper>
+            {[1, 1, 1, 1, 0].map(v => (
+              <I.Star width={27} height={27} fill={v ? "#FFD29B" : "#C4C4C4"} />
+            ))}
+            <S.StarText>4점</S.StarText>
+          </S.StarWrapper>
+        </S.OrderCafeInfoWrapper>
+        <S.NavWrapper nav={nav}>
+          <S.NavItemWrapper onClick={() => setNav("menu")}>메뉴</S.NavItemWrapper>
+          <S.NavItemWrapper onClick={() => setNav("review")}>리뷰</S.NavItemWrapper>
+        </S.NavWrapper>
+        {nav === "menu" ? <MenuList menuList={menuList} /> : <ReviewList />}
+      </S.Wrapper>
+    </S.Positioner>
   );
 };
 
--- containers/OrderContainer/index.tsx
+++ containers/OrderContainer/index.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Order } from "../../components";
 
 const OrderContainer: React.FC = () => {
-  return <Order />;
+  const [nav, setNav] = useState<string>("menu");
+  const Menus = [
+    { menuId: 1, menuName: "나이트로 바닐라 크림", menuPrice: 5600, menuImage: "" },
+    { menuId: 2, menuName: "나이트로 바닐라 크림", menuPrice: 5600, menuImage: "" },
+    { menuId: 3, menuName: "나이트로 바닐라 크림", menuPrice: 5600, menuImage: "" },
+    { menuId: 4, menuName: "나이트로 바닐라 크림", menuPrice: 5600, menuImage: "" },
+    { menuId: 5, menuName: "나이트로 바닐라 크림", menuPrice: 5600, menuImage: "" },
+    { menuId: 6, menuName: "나이트로 바닐라 크림", menuPrice: 5600, menuImage: "" },
+  ];
+  const Reviews = [
+    { reviewStar: 4.5, reviewContent: "사장님이 친절하세요^^" },
+    { reviewStar: 4.5, reviewContent: "사장님이 친절하세요^^" },
+    { reviewStar: 4.5, reviewContent: "사장님이 친절하세요^^" },
+    { reviewStar: 4.5, reviewContent: "사장님이 친절하세요^^" },
+  ];
+  return <Order menuList={Menus} nav={nav} setNav={setNav} />;
 };
 
 export default OrderContainer;
