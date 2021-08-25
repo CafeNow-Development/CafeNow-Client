15b4f28b750cf16b3ce15bd925880d8afe73fda7
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-08-25 11:47:08 +0900

    chore: image slider setup

---
+++ components/ImageSlider/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from "./style";
+
+interface ImageSliderProps {}
+
+const ImageSlider: React.FC<ImageSliderProps> = () => {
+  return (
+    <S.ImageSliderWrapper>
+      <div>ImageSlider</div>
+    </S.ImageSliderWrapper>
+  );
+};
+
+export default ImageSlider;
---
+++ components/ImageSlider/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ImageSliderWrapper = styled.div`
+    width : 100%fit-content;
+    height : 30vh;
+
+    background-color: skyblue;
+`;
+
