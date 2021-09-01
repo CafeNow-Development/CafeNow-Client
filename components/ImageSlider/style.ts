d2eba18d28a133e428c51330770117760ee5e3ec
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-09-01 10:14:49 +0900

    feat: imageslider style 작성

--- components/ImageSlider/style.ts
+++ components/ImageSlider/style.ts
@@ -1,9 +1,25 @@
 import styled from "styled-components";
 
-export const ImageSliderWrapper = styled.div`
+export const ImageSliderWrapper = styled.div<{imageItem: string}>`
     width : 100%fit-content;
     height : 30vh;
 
-    background-color: skyblue;
+    background: url(${props => props.imageItem});
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
+export const SliderBtn = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    text-align: center;
+    background-color: rgba(1,1,1,0.3);
+
+    padding: 10px 15px;
+
+    font-size: 15px;
+`;
